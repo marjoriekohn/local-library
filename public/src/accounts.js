ebadd4c57353dfcf7a6ce4b58dfe5c08db36c0cd
Marjorie Kohn <marjoriekohn01@gmail.com>
2023-09-13 17:30:46 -0400

    account page passed tests

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,31 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) => {
+    return accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1;
+  })
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) { // return number of books borrowed - borrowing 1 book 10 times = 1
+  return books.reduce((total, book) => {
+    if (book.borrows.some((borrow) => borrow.id === account.id)) {
+      total += 1
+    }
+    return total;
+  }, 0);
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  return books.filter((book) =>
+    book.borrows.some((borrow) => borrow.id === account.id && !borrow.returned)
+  ).map((book) => {
+    const author = authors.find((author) => author.id === book.authorId);
+    return {...book, author};
+  });
+}
+
 
 module.exports = {
   findAccountById,
